494431d9bcf8b1254ee4562a3b30bbd428942d9f
zhenggh <work345@qq.com>
2017-09-08 18:33:12 +0800

    add str

--- samples/zhangsantest/special_str.py
+++ samples/zhangsantest/special_str.py
@@ -4,6 +4,8 @@
 class Student(object):
     def __init__(self,name):
         self.name=name
-
+    def __str__(self):
+        return 'Student object (name:%s)' %self.name
 
 print(Student('mike'))
+
